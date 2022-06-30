b4abb6af5969109ab4917e12861d501fd4fa0030
isra.dev@outlook.com <isra.dev@outlook.com>
2022-06-30 16:34:24 -0500

    Final commit

---
+++ 7. Modulos ejecutando modulos como scripts/Ejercicio_1/main.py
@@ -0,0 +1,7 @@
+#import operaciones as op
+import operaciones as op
+
+op.sumar(8, 12)
+op.restar(50, 25)
+op.multiplicar(3, 10)
+op.dividir(70, 2)
---
+++ 7. Modulos ejecutando modulos como scripts/Ejercicio_1/operaciones.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    res = a + b
+    print(a, "+", b, "=", res)
+def restar(a, b):
+    res = a - b
+    print(a, "-", b, "=", res)
+
+def multiplicar(a, b):
+    res = a * b
+    print(a, "*", b, "=", res)
+
+def dividir(a, b):
+    res = a / b
+    print(a, "/", b, "=", res)
