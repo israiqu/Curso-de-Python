4baa21dfa4f9df8e6e4e7133a783e3e750427949
isra.dev@outlook.com <isra.dev@outlook.com>
2022-07-01 00:44:22 -0500

    First commit

---
+++ 9. Introduccion a la biblioteca estandar de Python y funciones Built-in/Ejercicio_1/main.py
@@ -0,0 +1,6 @@
+
+saludo = input('¿Como te llamas? ')
+print(f'Hola {saludo}, vamos a comenzar')
+
+#paises = input("Escribe el nombre de 3 paises")
+#print(f'Los paises seleccionados son {paises}')
