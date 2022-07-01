99ce3b0deed5d000b1b3abc33da92765fe7abf14
isra.dev@outlook.com <isra.dev@outlook.com>
2022-06-30 23:31:01 -0500

    Final commit

--- 8. Entrada y salida/Ejercicio_1/main.py
+++ 8. Entrada y salida/Ejercicio_1/main.py
@@ -1,5 +1,12 @@
 f = open('mi_nota.txt', 'w')
+f.write('Ejercicio #2\n\n')
+f.close()
 
-f.write('Ejercicio #2')
+f = open('mi_nota.txt', 'r+')
+f.readline()
+f.write('En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.\n')
+f.close()
 
+f.seek(0)
+print(f.read())
 f.close()
